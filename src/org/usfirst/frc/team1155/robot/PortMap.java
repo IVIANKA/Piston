bad013a6d031aab9880ab0c67d814d155de581e1
Karun Ellango <ellangok@bxscience.edu>
2017-01-19 17:02:06 -0500

    Created the Portmap

---
+++ src/org/usfirst/frc/team1155/robot/PortMap.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1155.robot;
+
+public class PortMap {
+	public static final int SolenoidF = 1;
+	public static final int SolenoidR = 2;
+	public static final int Solenoid2F = 3;
+	public static final int Solenoid2R = 4;
+
+	public static final int xboxController = 5;
+	public static final int xboxControllerX = 9;
+	public static final int xboxControllerY = 10;
+}
